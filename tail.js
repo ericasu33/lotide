ee283a8fad34cdfea208520241978013054ab022
Erica <ericasu33@gmail.com>
2020-10-26 22:47:17 +0000

    created some test case with assertEqual

--- tail.js
+++ tail.js
@@ -12,10 +12,20 @@ const tail = (arr) => {
   return arr.slice(1);
 };
 
+let result = tail(["Hello", "Lighthouse", "Labs"]);
+result = result.join();
+console.log(result);
+assertEqual(result, ["Lighthouse", "Labs"].join()); 
+
+const result2 = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result2.length, 2); // ensure we get back two elements
+assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+
 // const words = [];
 // console.log(tail(words))
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
