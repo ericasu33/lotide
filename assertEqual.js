5e3d59b74556f51a3c70708301f560396cf80f84
Erica <ericasu33@gmail.com>
2020-10-26 22:07:37 +0000

    basic function informing whether expected and actual value inputted is equal or not and return a message

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😢😢😢 Asserion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(32, 21);
