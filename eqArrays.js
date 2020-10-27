909dc5464c896428433c180760a7020614358426
Erica <ericasu33@gmail.com>
2020-10-27 22:25:05 +0000

    modified spacing and added semicolon

--- eqArrays.js
+++ eqArrays.js
@@ -7,24 +7,24 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (actual, expected) => {
-let compare = false;
+  let compare = false;
 
   if (actual.length !== expected.length) {
     compare = false;
   } else {
-    for (let i = 0; i < actual.length; i++){
-      compare = actual[i] === expected[i]
-      }
+    for (let i = 0; i < actual.length; i++) {
+      compare = actual[i] === expected[i];
     }
+  }
     
-return compare;
+  return compare;
 };
 
-console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])) //false;
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-console.log(eqArrays([1,2,3], [1,2,4])) //false
+console.log(eqArrays([1,2,3], [1,2,4])); //false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
