ba38f7c1a42b160f985c0407477e123ee0fe6f46
Erica <ericasu33@gmail.com>
2020-10-27 23:32:15 +0000

    refractored further

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,22 @@
 const assertArraysEqual = (actual, expected) => {
-  let compare = false;
+  let compare = true;
   
   if (actual.length !== expected.length) {
     compare = false;
   } else {
     for (let i = 0; i < actual.length; i++) {
-      compare === true ? 
-      compare = actual[i] === expected[i]:
-      compare = false
+      if (actual[i] !== expected[i]) {
+        compare = false;
+        break;
+      } 
     }
   }
+
   compare ?
     console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`) :
     console.log(`😡😡😡 Asserion Failed: ${actual} !== ${expected}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]); // => fail
+assertArraysEqual([1, 2, 3], [1, 2, 4, 5]); // => fail
 assertArraysEqual([1, 2, 3], [2, 3, 3]); // => fail
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => pass
