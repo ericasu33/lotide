b8fab0145f24e8b0c34d28c4737f760e3f01010a
Erica <ericasu33@gmail.com>
2020-10-27 23:32:20 +0000

    refractored further

--- eqArrays.js
+++ eqArrays.js
@@ -7,33 +7,27 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (actual, expected) => {
-  let compare = false;
-
+  // let compare = true;
   if (actual.length !== expected.length) {
-    compare = false;
-  } else {
-    // console.log(`the compare value now: ${compare}`);
-    for (let i = 0; i < actual.length; i++) {
-      compare === true ? 
-      compare = actual[i] === expected[i]:
-      compare = false
-      // console.log(`actual: ${actual[i]}`)
-      // console.log(`expected: ${expected[i]}`);
-      // console.log(compare);
-      // if (compare === false){
-      //   // console.log("I worked");
-      //   break;
-    }
+    // compare = false;
+    return false;
   }
-    
-  return compare;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      // compare = false;
+      // break;
+      return false;
+    } 
+  }
+
+  return true;
 };
 
-// console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 3, 3])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-// console.log(eqArrays([1,2,3], [1,2,4])); //false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+console.log(eqArrays([1,2,3], [1,2,4])); //false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
