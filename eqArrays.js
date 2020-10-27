187b04dba7efa99cee200adba0783c4ff9834a5b
Erica <ericasu33@gmail.com>
2020-10-27 23:09:08 +0000

    update if statement

--- eqArrays.js
+++ eqArrays.js
@@ -12,19 +12,28 @@ const eqArrays = (actual, expected) => {
   if (actual.length !== expected.length) {
     compare = false;
   } else {
+    // console.log(`the compare value now: ${compare}`);
     for (let i = 0; i < actual.length; i++) {
-      compare = actual[i] === expected[i];
+      compare === true ? 
+      compare = actual[i] === expected[i]:
+      compare = false
+      // console.log(`actual: ${actual[i]}`)
+      // console.log(`expected: ${expected[i]}`);
+      // console.log(compare);
+      // if (compare === false){
+      //   // console.log("I worked");
+      //   break;
     }
   }
     
   return compare;
 };
 
-console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 3, 3])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-console.log(eqArrays([1,2,3], [1,2,4])); //false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// console.log(eqArrays([1,2,3], [1,2,4])); //false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
