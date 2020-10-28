a1cebe8264cf3d1afb98e7744d22267b8aebcfa3
Erica <ericasu33@gmail.com>
2020-10-28 21:41:44 +0000

    counts how many letters in a given string by returning the corresponding letter and count in an object

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Asserion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (sentence) => {
+  let noSpace = sentence.split(" ").join("");
+
+  let results = {};
+  for (const letter of noSpace) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  
+  return results;
+};
+
+const results1 = countLetters("light house in the house");
+
+assertEqual(results1["l"], 1);
+assertEqual(results1["h"], 4);
+assertEqual(results1["e"], 3);
