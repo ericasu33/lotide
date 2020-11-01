87705e7f7356051262224f67c9cfd9cf71e12e39
Erica <ericasu33@gmail.com>
2020-11-01 06:15:13 +0000

    added recursive for nested array

--- eqArrays.js
+++ eqArrays.js
@@ -7,27 +7,29 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (actual, expected) => {
-  // let compare = true;
+  let output = true;
   if (actual.length !== expected.length) {
-    // compare = false;
     return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      // compare = false;
-      // break;
+  } else for (let i = 0; i < actual.length; i++) {
+    if (Array.isArray(actual)) {
+      output = output && eqArrays(actual[i], expected[i]);
+    } else if (actual[i] !== expected[i]) {
       return false;
     }
   }
-
-  return true;
+  return output;
 };
 
-console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 3, 3])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays([[2, 3], 4], [[2, 3], [4]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
 
-console.log(eqArrays([1,2,3], [1,2,4])); //false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 3, 3])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+// console.log(eqArrays([1,2,3], [1,2,4])); //false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
