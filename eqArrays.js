96f8a8379059fa7f4f3e8c22e252ae385ab249d5
Erica <ericasu33@gmail.com>
2020-11-02 19:00:46 +0000

    refractored so testing is done on separate file wz use of module n require

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,11 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡 Asserion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (actual, expected) => {
   let output = true;
   if (actual.length !== expected.length) {
     return false;
-  } else for (let i = 0; i < actual.length; i++) {
-    if (Array.isArray(actual)) {
+  }
+  
+  for (let i = 0; i < actual.length; i++) {
+    if (Array.isArray(actual[i])) {
       output = output && eqArrays(actual[i], expected[i]);
     } else if (actual[i] !== expected[i]) {
       return false;
@@ -20,16 +14,4 @@ const eqArrays = (actual, expected) => {
   return output;
 };
 
-console.log(eqArrays([[2, 3], 4], [[2, 3], [4]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
-
-// console.log(eqArrays(["1", "2", 3], ["3","2","1","1"])); //false;
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 3, 3])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-// console.log(eqArrays([1,2,3], [1,2,4])); //false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
