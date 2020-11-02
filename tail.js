50f4c5dc2f1efcadf9449a5072e8113420b1ac9b
Erica <ericasu33@gmail.com>
2020-11-02 17:16:36 +0000

    refractored so testing is done on separate file wz use of module n require

--- tail.js
+++ tail.js
@@ -1,31 +1,10 @@
 //tail = every element except the head of the array
-
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡😡😡 Asserion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-result = result.join();
-console.log(result);
-assertEqual(result, ["Lighthouse", "Labs"].join()); 
+module.exports = tail;
 
-const result2 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result2.length, 2); // ensure we get back two elements
-assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
 
 // const words = [];
 // console.log(tail(words))
-
-// // Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
