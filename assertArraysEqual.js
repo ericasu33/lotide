64d4a2b45832cf709a5aea5063d2ec3385eeaab5
Erica <ericasu33@gmail.com>
2020-11-02 19:15:22 +0000

    refractored so testing is done on separate file wz use of module n require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (actual, expected) => {
   eqArrays(actual, expected) ?
@@ -17,7 +6,4 @@ const assertArraysEqual = (actual, expected) => {
     console.log(`😡😡😡 Asserion Failed: ${actual} !== ${expected}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 4, 5]); // => fail
-assertArraysEqual([1, 2, 3], [2, 3, 3]); // => fail
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => pass
-assertArraysEqual([1], [1]); // => pass
+module.exports = assertArraysEqual;
